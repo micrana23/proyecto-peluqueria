4fa7f70392f2707b8fe202fbb201fbfd505cc39a
micrana <micrana23@hotmail.com>
2026-08-03 13:46:28 +0200

    Optimiza recursos y mejora el SEO del frontend

--- peluqueria-frontend/src/App.jsx
+++ peluqueria-frontend/src/App.jsx
@@ -1,29 +1,32 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Servicios from "./pages/Servicios";
-import Contacto from "./pages/Contacto";
-import Peluqueria from "./pages/Peluqueria";
-import Novias from "./pages/Novias";
-import Unas from "./pages/Unas";
-import NotFound from "./pages/NotFound";
 import "./styles/global.css";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
+
+const Home = lazy(() => import("./pages/Home"));
+const Servicios = lazy(() => import("./pages/Servicios"));
+const Contacto = lazy(() => import("./pages/Contacto"));
+const Peluqueria = lazy(() => import("./pages/Peluqueria"));
+const Novias = lazy(() => import("./pages/Novias"));
+const Unas = lazy(() => import("./pages/Unas"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const PrivacyPolicy = lazy(() => import("./pages/PrivacyPolicy"));
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/servicios" element={<Servicios />} />
-          <Route path="/servicios/peluqueria" element={<Peluqueria />} />
-          <Route path="/servicios/novias" element={<Novias />} />
-          <Route path="/servicios/unas" element={<Unas />} />
-          <Route path="/contacto" element={<Contacto />} />
-          <Route path="/politica-privacidad" element={<PrivacyPolicy />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense fallback={<div>Cargando...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/servicios" element={<Servicios />} />
+            <Route path="/servicios/peluqueria" element={<Peluqueria />} />
+            <Route path="/servicios/novias" element={<Novias />} />
+            <Route path="/servicios/unas" element={<Unas />} />
+            <Route path="/contacto" element={<Contacto />} />
+            <Route path="/politica-privacidad" element={<PrivacyPolicy />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </div>
     </Router>
   );
