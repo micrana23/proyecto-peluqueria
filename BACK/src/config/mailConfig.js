19371e3b354ea548d74c465757184ff02427a59a
micrana <micrana23@hotmail.com>
2026-09-26 14:34:13 +0200

    Fuerza IPv4 en conexion SMTP para evitar timeout en Render

--- BACK/src/config/mailConfig.js
+++ BACK/src/config/mailConfig.js
@@ -1,7 +1,10 @@
 import { env } from "./env.js";
 
 const mailConfig = {
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4, // fuerza IPv4, evita el timeout típico de Render con IPv6
   auth: {
     user: env.emailUser,
     pass: env.emailPass,
